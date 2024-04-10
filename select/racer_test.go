1df92a2b4cd7624b818d685fdffc601b474736b0
Chris James <quii@hey.com>
2024-04-10 07:53:34 +0100

    select chapter

---
+++ select/racer_test.go
@@ -0,0 +1,42 @@
+package _select
+
+import (
+	. "github.com/quii/pepper"
+	. "github.com/quii/pepper/matchers/comparable"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares the speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		winner, err := Racer(fastServer.URL, slowServer.URL)
+
+		ExpectNoError(t, err)
+		Expect(t, winner).To(Equal(fastServer.URL))
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+		ExpectError(t, err)
+	})
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
